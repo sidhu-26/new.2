1120bba52a4d161f118615fd81e26fe68b008987
Siddaarth <siddaarth26@gmail.com>
2025-04-06 23:06:05 +0530

    Update views.py

--- studios/views.py
+++ studios/views.py
@@ -10,24 +10,25 @@ def harvesine(lat1, lon1, lat2, lon2):
     dlat = lat2 - lat1
     a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
     c = 2 * asin(sqrt(a))
-    r = 6378  
+    r = 6371  
     return c * r
 
 @api_view(['GET'])
 def nearest_std(request):
     try:
         user_latitude = float(request.GET.get('latitude'))
-        user_logitude = float(request.GET.get('longitude'))
+        user_longitude = float(request.GET.get('longitude'))
     except (TypeError, ValueError):
         return Response({'error': 'Check latitude and longitude values.'}, status=400)
 
-    SerpApiKey = "" # use serp api key , key is available in readme
+    SerpApiKey = "" # use a p i key , (key available in readme)
     url = "https://serpapi.com/search.json"
 
     params = {
         "engine": "google_maps",
         "q": "dance studio",
-        "ll": f"@{user_latitude},{user_logitude},15z",
+        "ll": f"@{user_latitude},{user_longitude},15z",
+        "location": f"{user_latitude},{user_longitude}",
         "google_domain": "google.co.in",
         "radius": 4000,
         "api_key": SerpApiKey
@@ -45,7 +46,7 @@ def nearest_std(request):
             "address": studio.get("address", "Address not available"),
             "rating": studio.get("rating", "Not available"),
             "distance_KM": round(harvesine(
-                user_latitude, user_logitude,
+                user_latitude, user_longitude,
                 studio["gps_coordinates"]["latitude"],
                 studio["gps_coordinates"]["longitude"]
             ), 2)
@@ -53,6 +54,8 @@ def nearest_std(request):
         for studio in data["local_results"]
     ]
 
-    studios.sort(key=lambda x: x['distance_KM'])
+    filtered_studios = [studio for studio in studios if studio["distance_KM"] <= 4]
 
-    return Response({"studios": studios})
+    filtered_studios.sort(key=lambda x: x['distance_KM'])
+
+    return Response({"studios": filtered_studios})
